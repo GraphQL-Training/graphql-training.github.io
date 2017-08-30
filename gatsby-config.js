cba62555f8912efce2754a5ad15f867289b05aea
Jof Arnold <public@jofarnold.com>
2017-08-30 19:22:25 +0100

    Tweak config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,30 +7,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: `pages`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/data`,
-        name: `data`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          "gatsby-remark-autolink-headers",
-          "gatsby-remark-images",
-          "gatsby-remark-prismjs",
-        ],
-      },
-    },
     `gatsby-transformer-json`,
   ],
 };
