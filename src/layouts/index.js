7ad3f02ed0ad8eaf41800628b9c85b8774008191
Jof Arnold <public@jofarnold.com>
2017-08-30 19:44:31 +0100

    Bootstrap simple layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default ({ children }) => (
   <div>
     <header>
-      <nav>Nav</nav>
+      <nav />
     </header>
     <section>{children()}</section>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "../layouts/index.scss";
+import "../layouts/flexgrid.scss";
 
 export default () => (
-  <div>
-    <h1>Hello world!</h1>
+  <div className="container mw8 flex center-xs middle-xs">
+    <div className="column">
+      <h1>Coming soon...</h1>
+      <p>
+        In the meantime why not check out our open source project{" "}
+        <a href="https://graphile.org" title="Graphile">
+          graphile.org
+        </a>.
+      </p>
+    </div>
   </div>
 );
