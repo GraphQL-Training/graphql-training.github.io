4bc097376523036dd56a93ab2a519e8017dae7a5
Jof Arnold <public@jofarnold.com>
2017-09-04 14:34:37 +0100

    Bootstrap some more design.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,50 @@ import React from "react";
 import "../layouts/index.scss";
 import "../layouts/flexgrid.scss";
 
-export default () => (
-  <div className="container mw8 flex center-xs middle-xs">
-    <div className="column">
-      <h1>Coming soon...</h1>
-      <p>
-        In the meantime why not check out our open source project{" "}
-        <a href="https://graphile.org" title="Graphile">
-          graphile.org
-        </a>.
-      </p>
+const HomeHeader = () => (
+  <div className="bg-off-grey">
+    <div className="header br-curve-section flex justify-center flex-column bg-off-grey">
+      <div className="container mw8 w-100">
+        <div className="row justify-between">
+          <div className="pt1vh pb1vh col-xs-7">
+            <h1>GraphQL Training</h1>
+            <h3>
+              Training in London, Berlin, Paris
+              <br />
+              and beyond from the creator
+              <br /> of PostGraphQL
+            </h3>
+          </div>
+
+          <div className="col-xs-3 header-brain">Brain</div>
+        </div>
+      </div>
+      <div className="header-spacer" />
     </div>
   </div>
 );
+
+const Index = () => (
+  <div>
+    <HomeHeader />
+    <section className="bg-off-grey">
+      <div className="container mw8 w-100">
+        <div className="row">
+          <div className="col-xs-12 tc flex flex-column items-center mt4 mb4">
+            <h3>
+              Nothing but awesome. You're getting some serious expertise here!
+            </h3>
+            <p>
+              In the meantime why not check out our open source project{" "}
+              <a href="https://graphile.org" title="Graphile">
+                graphile.org
+              </a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+);
+
+export default Index;
