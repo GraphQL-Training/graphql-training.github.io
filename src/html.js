21edef93357c17a53cd78ea94760b684b35989fe
Jof Arnold <public@jofarnold.com>
2017-09-08 12:29:57 +0100

    CSS caching

--- src/html.js
+++ src/html.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const ANALYTICS_KEY = "";
+const buildTimestamp = new Date().toISOString().replace(/:/g, "-");
 
 const Html = ({ postBodyComponents, body }) => (
   <html lang="en">
     <head>
       <script src="https://use.fontawesome.com/c72bfae6f9.js" />
       <meta charSet="utf-8" />
+      <link
+        rel="stylesheet"
+        href={`/styles.css?t=${encodeURIComponent(buildTimestamp)}`}
+      />
     </head>
     <body>
       <div id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
