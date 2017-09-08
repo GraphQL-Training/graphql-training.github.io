f73f28e94e85048dacd1a31b18f2ce4465c4984c
Jof Arnold <public@jofarnold.com>
2017-09-08 12:28:59 +0100

    Add font awesome

--- src/html.js
+++ src/html.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+const ANALYTICS_KEY = "";
+
 const Html = ({ postBodyComponents, body }) => (
   <html lang="en">
     <head>
+      <script src="https://use.fontawesome.com/c72bfae6f9.js" />
       <meta charSet="utf-8" />
     </head>
     <body>
