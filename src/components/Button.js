4f9ff28f7d42d6f1bbc4051829fc5acf22165fd1
Jof Arnold <github@jofarnold.com>
2017-09-19 14:16:43 +0100

    Add title prop to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-const ButtonLink = ({ href, children }) => (
+const ButtonLink = ({ href, children, title }) => (
   <a
     className="b--white b--solid bw1 ph3 ph4-ns pv2 pv3-ns f5 f4-ns ttu white tracked-light dib fw6 no-underline-hover grow"
     href={href}
+    title={title || "Link"}
   >
     {children}
   </a>
 );
 
-const ButtonLinkSolid = ({ href, children }) => (
+const ButtonLinkSolid = ({ href, children, title }) => (
   <a
     href={href}
     className="b--white b--solid bw1 ph3 ph4-ns pv2 pv3-ns f5 f4-ns ttu fire tracked-light dib fw6 no-underline-hover grow shadow--l2 bg-white"
+    title={title || "Link"}
   >
     {children}
   </a>
