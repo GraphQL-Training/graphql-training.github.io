7b3ae4873bc9f8d219719c065367cd01de73eb7d
Jof Arnold <github@jofarnold.com>
2017-10-13 15:11:47 +0100

    Add more elements to reuse copy

--- src/components/PageElements.js
+++ src/components/PageElements.js
@@ -24,4 +24,87 @@ const HeaderLink = ({ faName, children, href, title, pushRight }) => (
   </li>
 );
 
-export { CourseTitle, TitledParagraph, HeaderLink };
+const FrontendCoursePoints = () => (
+  <div className="row">
+    <div className="col-xs-12 col-md-6">
+      <ul>
+        <li>GraphQL concepts and terminology.</li>
+        <li>How to perform queries and mutations using GraphiQL and HTTP.</li>
+        <li>Composing GraphQL documents with fragments.</li>
+        <li>
+          How to integrate the Apollo GraphQL client into React applications
+        </li>
+      </ul>
+    </div>
+    <div className="col-xs-12 col-md-6">
+      <ul>
+        <li>Simplification: removing unnecessary state from your app.</li>
+        <li>Authenticating GraphQL requests.</li>
+        <li>Debugging techniques.</li>
+        <li>Optimistic updates and other performance and UX optimizations.</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const FrontendCourseDescription = () => (
+  <div className="col-xs-12 flex flex-column items-center tc">
+    <CourseTitle>Frontend GraphQL with React</CourseTitle>
+    <p>
+      This 2 day course gives React developers the experience and intuitions to
+      create GraphQL applications rapidly and on the right footing. Starting
+      with learning the core concepts and how to use queries, mutations and
+      fragments, the course progresses to building advanced skills in areas such
+      as authentication, caching, optimistic updates and debugging.
+    </p>
+  </div>
+);
+
+const FullstackCoursePoints = () => (
+  <div className="row">
+    <div className="col-xs-12 col-md-6">
+      <ul>
+        <li>GraphQL concepts and terminology.</li>
+        <li>How to perform queries and mutations using GraphiQL and HTTP.</li>
+        <li>Composing GraphQL documents with fragments.</li>
+        <li>
+          How to integrate the Apollo GraphQL client into React applications
+        </li>
+      </ul>
+    </div>
+    <div className="col-xs-12 col-md-6">
+      <ul>
+        <li>Simplification: removing unnecessary state from your app.</li>
+        <li>Authenticating GraphQL requests.</li>
+        <li>Debugging techniques.</li>
+        <li>Optimistic updates and other performance and UX optimizations.</li>
+      </ul>
+    </div>
+  </div>
+);
+
+const FullstackCourseDescription = () => (
+  <div className="col-xs-12 flex flex-column items-center tc">
+    <CourseTitle>GraphQL for Fullstack Developers & Teams</CourseTitle>
+    <p>
+      This advanced 2 day course gives full stack engineers and cross-discipline
+      teams the experience and intuitions to create high performance GraphQL
+      applications/APIs rapidly and on the right footing.
+    </p>
+    <p>
+      Starting with learning the core concepts and how to use queries, mutations
+      and fragments, the course progresses to building skills in areas such as
+      schema design, authentication and API performance.
+    </p>
+  </div>
+);
+
+export {
+  CourseTitle,
+  TitledParagraph,
+  HeaderLink,
+  FrontendCourseDescription,
+  FrontendCoursePoints,
+  FullstackCoursePoints,
+  FullstackCourseDescription,
+};
